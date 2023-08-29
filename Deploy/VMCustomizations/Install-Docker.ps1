25af420879eaf842837a49e16ebf437fa3bedcf9
Evilazaro <evalvesd@microsoft.com>
2023-08-29 17:02:41 -0500

    Fixing PowerShell Script

--- Deploy/VMCustomizations/Install-Docker.ps1
+++ Deploy/VMCustomizations/Install-Docker.ps1
@@ -1,6 +1,8 @@
-$URL="https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?_gl=1*h05men*_ga*NzE2MTUzMjUzLjE2OTMwMDE0NTA.*_ga_XJWPQMJYHQ*MTY5MzMyNTgzMC4yLjEuMTY5MzMyNTk3NC41OS4wLjA."
-$PATH="C:\Temp\DockerDesktop.exe"
+$URL="https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?_gl=1*h05men*_ga*NzE2MTUzMjUzLjE2OTMwMDE0NTA.*_ga_XJWPQMJYHQ*MTY5MzMyNTgzMC4yLjEuMTY5MzMyNTk3NC41OS4wLjA.";
+$PATH="C:\Temp\DockerDesktop.exe";
 
-mkdir C:\Temp
+mkdir C:\Temp;
 
-Invoke-WebRequest -Uri $URL -OutFile $PATH
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+
+Invoke-WebRequest -Uri $URL -OutFile $PATH;
