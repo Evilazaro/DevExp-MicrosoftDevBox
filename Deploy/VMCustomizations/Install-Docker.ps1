034c6a45b4b0494a02ac6c8cb9ff256b8b0cad07
Evilazaro <evalvesd@microsoft.com>
2023-09-21 08:35:38 -0500

    Removing PowerShell

--- Deploy/VMCustomizations/Install-Docker.ps1
+++
@@ -1,8 +0,0 @@
-$URL="https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?_gl=1*h05men*_ga*NzE2MTUzMjUzLjE2OTMwMDE0NTA.*_ga_XJWPQMJYHQ*MTY5MzMyNTgzMC4yLjEuMTY5MzMyNTk3NC41OS4wLjA.";
-$PATH="C:\Temp\DockerDesktop.exe";
-
-mkdir C:\Temp;
-
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-
-Invoke-WebRequest -Uri $URL -OutFile $PATH;
