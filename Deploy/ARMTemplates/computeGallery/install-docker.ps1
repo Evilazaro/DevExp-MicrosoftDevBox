d15cb0ccaf50589e1f72bcc8f464a65bf2ca978b
Evilazaro Alves <evalvesd@microsoft.com>
2023-09-29 17:13:39 -0500

    checksums

--- Deploy/ARMTemplates/computeGallery/install-docker.ps1
+++ Deploy/ARMTemplates/computeGallery/install-docker.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force;
-choco install wsl-ubuntu-2204 --params "/AutomaticInstall:true";
+choco install -y wsl-ubuntu-2204 --ignore-checksums --params "/AutomaticInstall:true";
 choco install -y docker-desktop --ignore-checksums --ia '--quiet --accept-license';
