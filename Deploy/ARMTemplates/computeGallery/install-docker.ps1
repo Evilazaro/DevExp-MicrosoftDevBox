22ff336038ed640672331a84de7a783eafaa1099
Evilazaro Alves <evalvesd@microsoft.com>
2023-10-02 11:01:29 -0500

    PowerShell customizations script updates

--- Deploy/ARMTemplates/computeGallery/install-docker.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-choco install -y --ignore-checksums wsl-ubuntu-2204;
-choco install -y --ignore-checksums docker-desktop  --ia '--quiet --accept-license';
