8e20f7b76295df1049edbcb30e6d8fc2ccbd24c3
Evilazaro Alves <evalvesd@microsoft.com>
2023-10-09 23:18:48 -0400

    update powershell script

--- Deploy/ARMTemplates/computeGallery/eShopBackEnd-Customizations.ps1
+++ Deploy/ARMTemplates/computeGallery/eShopBackEnd-Customizations.ps1
@@ -101,7 +101,7 @@ function Install-VSCodeExtensions {
 function installAzureADModules{
     Write-Output "Installing Azure AD Modules"
     try {
-        Install-Module -Name AzureAD -Force
+        Install-Module -Name AzureAD -Force -SkipPublisherCheck
     } catch {
         throw "Failed to install Azure AD Modules"
     }
