cb3b4671fc6e21bb310c15816c5971a7c7361e52
Evilazaro Alves <evalvesd@microsoft.com>
2023-10-10 12:32:11 -0400

    Adding users to docker-user group

--- Deploy/ARMTemplates/computeGallery/eShopBackEnd-Customizations.ps1
+++ Deploy/ARMTemplates/computeGallery/eShopBackEnd-Customizations.ps1
@@ -85,9 +85,6 @@ try {
     installAzureADModules
     Install-VSCodeExtensions
     Install-DockerDesktop
-    # Adds the current user to the docker-users group
-    Write-Host "Adding current user to docker-users group"
-    Add-LocalGroupMember -Group 'docker-users' -Member 'Everyone';
     Write-Output "Script completed successfully"
 } catch {
     Write-Error $_.Exception.Message
