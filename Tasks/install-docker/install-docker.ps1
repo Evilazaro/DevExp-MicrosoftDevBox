429fb51dc6820518a56067593ccffdc2a614b83b
Evilazaro <evilazaro@gmail.com>
2024-07-04 13:54:38 -0500

    Postman installation

--- Tasks/install-docker/install-docker.ps1
+++ Tasks/install-docker/install-docker.ps1
@@ -1,8 +1,9 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; 
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
+
 $Choco = "$Env:ProgramData/chocolatey/choco.exe"
 
 if(-not (Test-Path "$Choco")){
-    Set-ExecutionPolicy Bypass -Scope Process -Force; 
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 Write-Host "Start to install docker desktop"
