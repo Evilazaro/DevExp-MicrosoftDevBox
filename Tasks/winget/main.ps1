129142220aa60faff0b28bfe377037637fac2523
Evilazaro <evilazaro@gmail.com>
2024-07-05 14:35:56 -0500

    WinGet Update

--- Tasks/winget/main.ps1
+++ Tasks/winget/main.ps1
@@ -1,13 +1,27 @@
 
 function Install-WinGet {
     $actionTaken = $false
+
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
+    Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+
     # check if the Microsoft.Winget.Client module is installed
-    if (!(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
-        Write-Host "Installing Microsoft.Winget.Client"
-        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Scope AllUsers
-        Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
+    if (!(Get-Module -ListAvailable -Name Winget)) {
+        Write-Host "Installing Winget"        
 
         Install-Module WinGet -Scope AllUsers -Force
+
+        Write-Host "Done Installing Winget"
+        $actionTaken = $true
+    }
+    else
+    {
+        Write-Host "Winget is already installed"
+    }
+
+    if (!(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
+        Write-Host "Installing Microsoft.Winget.Client"
+        
         Install-Module Microsoft.WinGet.Client -Scope AllUsers -Force
 
         Write-Host "Done Installing Microsoft.Winget.Client"
