841150dbafc15de3eef5efb8d725227be2b62488
Evilazaro <evilazaro@gmail.com>
2024-07-05 12:58:09 -0500

    Remove-Dependencies

--- Tasks/update-dependencies/remove-dependencies.ps1
+++ Tasks/update-dependencies/remove-dependencies.ps1
@@ -18,7 +18,7 @@ function Uninstall-GitHubCLI {
     # Check if GitHub CLI is installed
     if (Get-Command gh -ErrorAction SilentlyContinue) {
         # Uninstall GitHub CLI
-        winget uninstall --id GitHub.cli -e --silent --accept-package-agreements --accept-source-agreements
+        winget uninstall --id GitHub.cli -e --silent 
         Write-Host "GitHub CLI has been uninstalled successfully."
     }
     else {
@@ -29,9 +29,9 @@ function Uninstall-GitHubCLI {
 # This function uninstalls the Azure Developer CLI
 function Uninstall-AzureDeveloperCLI {
     # Check if Azure Developer CLI is installed
-    if (Get-Command azdev -ErrorAction SilentlyContinue) {
+    if (Get-Command azd -ErrorAction SilentlyContinue) {
         # Uninstall Azure Developer CLI
-        winget uninstall --id Microsoft.Azd -e --silent --accept-package-agreements --accept-source-agreements
+        winget uninstall --id Microsoft.Azd -e --silent 
         Write-Host "Azure Developer CLI has been uninstalled successfully."
     }
     else {
@@ -39,19 +39,13 @@ function Uninstall-AzureDeveloperCLI {
     }
 }
 
-# This function uninstalls the .NET workloads
-function Uninstall-DotNet {
-    Write-Host "Start to uninstall .NET workloads"
-    dotnet workload uninstall --all
-    Write-Host "End to uninstall .NET workloads"
-}
 
 # This function uninstalls WinGet (if possible)
 function Uninstall-WinGet {
     # Check if WinGet is installed
     if (Get-Command winget -ErrorAction SilentlyContinue) {
         Write-Host "WinGet is installed. Uninstalling it now."
-        winget uninstall --id Microsoft.Winget.Client -e --silent --accept-package-agreements --accept-source-agreements
+        winget uninstall --id Microsoft.Winget.Client -e --silent 
     }
     else {
         Write-Host "WinGet is not installed."
@@ -59,12 +53,11 @@ function Uninstall-WinGet {
 }
 
 # This function uninstalls all the dependencies
-function Uninstall-Dependencies {
-    Uninstall-WinGet
-    Uninstall-DotNet    
+function Uninstall-Dependencies {   
     Uninstall-AzureRM
     Uninstall-GitHubCLI
     Uninstall-AzureDeveloperCLI
+    Uninstall-WinGet
 }
 
 # The main function that uninstalls all dependencies
