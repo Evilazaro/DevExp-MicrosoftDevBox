44f4d8ffeda4c4afd38cd2d469b7273fb38a613c
Evilazaro <evilazaro@gmail.com>
2024-07-05 20:09:42 -0500

    dependencies

--- Tasks/update-dependencies/update-dependencies.ps1
+++ Tasks/update-dependencies/update-dependencies.ps1
@@ -49,9 +49,15 @@ function Install-WinGet {
     }
     else {
         Write-Host "WinGet is not installed. Installing it now."
-        $scriptUrl = "https://raw.githubusercontent.com/Evilazaro/MicrosoftDevBox/main/Tasks/winget/main.ps1"
-
-        Invoke-RestMethod -Uri $scriptUrl | Invoke-Expression
+        $progressPreference = 'silentlyContinue'
+        Write-Information "Downloading WinGet and its dependencies..."
+        Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+        Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+        Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+        Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+        Write-Host "WinGet has been installed successfully."
     }
 }
 
