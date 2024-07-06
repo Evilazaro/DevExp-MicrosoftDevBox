167d54809763f440c728ad2717aa09b57e8ed83b
Evilazaro <evilazaro@gmail.com>
2024-07-05 19:21:09 -0500

    winget update

--- Tasks/winget/main.ps1
+++ Tasks/winget/main.ps1
@@ -3,16 +3,13 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 
 function Install-WinGet {
     $progressPreference = 'silentlyContinue'
-    $oFile = "c:\Downloads\"
-
-    mkdir $oFile
     Write-Information "Downloading WinGet and its dependencies..."
-    Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile $oFile"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile $oFile"Microsoft.VCLibs.x64.14.00.Desktop.appx"
-    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile $oFile"Microsoft.UI.Xaml.2.8.x64.appx"
-    Add-AppxPackage $oFile"Microsoft.VCLibs.x64.14.00.Desktop.appx" 
-    Add-AppxPackage $oFile"Microsoft.UI.Xaml.2.8.x64.appx"
-    Add-AppxPackage $oFile"Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+    Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+    Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
 }
 
 Install-WinGet
