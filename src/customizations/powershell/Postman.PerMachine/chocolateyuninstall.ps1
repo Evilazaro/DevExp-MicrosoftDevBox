c1a01b6fb2d400b93035fd41edf7e14993d2cc9c
Evilázaro Alves <evilazaro@gmail.com>
2024-07-25 12:39:37 -0500

    removing unused scripts

--- src/customizations/powershell/Postman.PerMachine/chocolateyuninstall.ps1
+++
@@ -1,4 +0,0 @@
-$ApplicationName = 'Postman'
-Remove-Item "${env:SystemDrive}\$ApplicationName" -Recurse -Force
-Remove-Item "${env:Public}\Desktop\$ApplicationName.lnk" -Recurse -Force 
-Remove-Item  "${env:AllUsersProfile}\Microsoft\Windows\Start Menu\Programs\$ApplicationName.lnk" -Recurse -Force 
