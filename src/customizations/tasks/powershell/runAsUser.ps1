26d0949bae0b4fd08b4768fbbb8bbfff28d30716
Evilázaro Alves <evilazaro@gmail.com>
2024-07-31 12:17:47 -0500

    fixing powershell task scheduler bug

--- src/customizations/tasks/powershell/runAsUser.ps1
+++ src/customizations/tasks/powershell/runAsUser.ps1
@@ -11,7 +11,7 @@ $CleanupTask = "DevBoxCustomizationsPwshCleanup"
 # Reference: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables
 $ProgressPreference = 'SilentlyContinue'
 
-Start-Transcript -Path $env:TEMP\scheduled-task-customization.log -Append -IncludeInvocationHeader
+Start-Transcript -Path $env:TEMP\scheduled-task-customization-pwsh.log -Append -IncludeInvocationHeader
 
 Write-Host "Microsoft Dev Box - Customizations"
 Write-Host "----------------------------------"
@@ -19,3 +19,4 @@ Write-Host "Setting up scheduled tasks..."
 
 Remove-Item -Path "$($CustomizationScriptsDir)\$($LockFile)"
 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
