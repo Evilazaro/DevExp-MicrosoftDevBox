bed68f5325c5e5022a7795678f99303394e4b78d
Evilazaro <evilazaro@gmail.com>
2024-09-26 11:34:45 -0500

    dos2unix

---
+++ src/deploy/bicep/bash/deployResourcesOrganization.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Azure Resource Group Names Constants
+devBoxResourceGroupName="eShopPetDevBox-rg"
+networkResourceGroupName="eShopPetNetworkConnectivity-rg"
+managementResourceGroupName="eShopPetDevBoxManagement-rg"
+
+# Function to create an Azure resource group
+createResourceGroup() {
+    local resourceGroupName="$1"
+
+    if [[ -z "$resourceGroupName" || -z "$location" ]]; then
+        echo "Error: Missing required parameters."
+        echo "Usage: createResourceGroup <resourceGroupName> <location>"
+        return 1
+    fi
+
+    echo "Creating Azure Resource Group: $resourceGroupName in $location"
+
+    if az group create --name "$resourceGroupName" --location "$location" --tags "division=eShopPet-Platform" "Environment=Prod" "offer=eShopPet-DevWorkstation-Service" "Team=Engineering" "solution=ContosoFabricDevWorkstation"; then
+        echo "Resource group '$resourceGroupName' created successfully."
+    else
+        echo "Error: Failed to create resource group '$resourceGroupName'."
+        return 1
+    fi
+}
+
+# Function to deploy resources for the organization
+deployResourcesOrganization() {
+
+    createResourceGroup "$devBoxResourceGroupName"
+    createResourceGroup "$networkResourceGroupName"
+    createResourceGroup "$managementResourceGroupName"
+
+    demoScript
+}
+
+deployResourcesOrganization
